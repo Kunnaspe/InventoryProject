32220258e7032ef529e363dd68ea2a17ece1b2f1
Kunnaspe <122162227+Kunnaspe@users.noreply.github.com>
2026-03-02 01:04:04 -0500

    Fix comment formatting in urls.py

--- DjangoProject/urls.py
+++ DjangoProject/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import RedirectView
 urlpatterns = [
     # Path to the Django admin site
     path('admin/', admin.site.urls),
-    # Cognito's new console only accepts simple callback paths, so we register
+    # Cognito's new console only accepts simple callback paths, so register
     # /callback in Cognito and redirect here to the real allauth handler.
     path('callback', RedirectView.as_view(url='/accounts/oidc/cognito/login/callback/', query_string=True)),
     # django-allauth handles all OAuth/OIDC flows under /accounts/
